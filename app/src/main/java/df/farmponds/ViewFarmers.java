bcd49c74617d85beac3a547d56a340c05554e0dc
dftechteam <dftechteam2018@gmail.com>
2019-10-11 17:32:49 +0530

    Edit details uploadedd

--- app/src/main/java/df/farmponds/ViewFarmers.java
+++ app/src/main/java/df/farmponds/ViewFarmers.java
@@ -197,6 +197,7 @@ public class ViewFarmers extends AppCompatActivity {
     private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
     private String userChoosenTask;
     String str_selected_farmerID,str_selected_farmerID_forimagesaving;
+
     public static final String sharedpreferenc_farmerid = "sharedpreference_farmer_id";
     public static final String Key_FarmerID = "farmer_id";
     SharedPreferences sharedpref_farmerid_Obj;
